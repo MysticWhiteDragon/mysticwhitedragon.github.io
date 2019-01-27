a328ea5f2773bfd01048ba516f1958518d429ab3
MysticWhiteDragon <MysticWhiteDragon@users.noreply.github.com>
2019-01-27 19:32:30 +0900

    ALT Tool stuff

---
+++ menu.js
@@ -0,0 +1,40 @@
+document.writeln('	<nav class="primnav no-print">');
+document.writeln('		<ul>');
+document.writeln('			<li><a title="Office" href="#documents"><svg class="icon"><use xlink:href="#icon-th-large"></use></svg> Office Suite</a>');
+document.writeln('				<ul class="secnav">');
+document.writeln('					<li><a title="Writer" href="writer.html">Writer</a></li>');
+document.writeln('					<li><a title="Spreadsheets" href="sheets.html">Spreadsheets</a></li>');
+document.writeln('					<li><a title="Presentations" href="presentation.html">Presentations</a></li>');
+document.writeln('				</ul>');
+document.writeln('			</li>');
+document.writeln('			<li><a title="Documents" href="#documents"><svg class="icon"><use xlink:href="#icon-th-large"></use></svg> Document Templates</a>');
+document.writeln('				<ul class="secnav">');
+document.writeln('					<li><a title="Vocabulary Test" href="vocabularytest.html">Vocabulary Test<div class="tag">Size: B4</div></a></li>');
+document.writeln('				</ul>');
+document.writeln('			</li>');
+document.writeln('			<li><a title="GAMES"><svg class="icon"><use xlink:href="#icon-mail_outline"></use></svg> Games</a>');
+document.writeln('				<ul class="secnav">');
+document.writeln('					<li><a title="Classroom Clash" href="clash.html">Classroom Clash</a></li>');
+document.writeln('					<li><a title="Wheel of Knowledge" href="wheel.html">Wheel of Knowledge</a></li>');
+document.writeln('					<li><a title="Trivia" href="trivia.html">Trivia</a></li>');
+document.writeln('					<li><a title="English Card Game" href="cardgame.html">English Card Game<div class="tag">Size: B5</div></a></li>');
+document.writeln('				</ul>');
+document.writeln('			</li>');
+document.writeln('		</ul>');
+document.writeln('	</nav>');
+
+
+
+
+
+
+
+document.writeln('<svg style="position: absolute; width: 0; height: 0;" width="0" height="0" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">');
+
+
+document.writeln('<symbol id="icon-th-large" viewBox="0 0 30 32">');
+document.writeln('<title>th-large</title>');
+document.writeln('<path class="path1" d="M13.714 18.286v6.857c0 1.25-1.036 2.286-2.286 2.286h-9.143c-1.25 0-2.286-1.036-2.286-2.286v-6.857c0-1.25 1.036-2.286 2.286-2.286h9.143c1.25 0 2.286 1.036 2.286 2.286zM13.714 4.571v6.857c0 1.25-1.036 2.286-2.286 2.286h-9.143c-1.25 0-2.286-1.036-2.286-2.286v-6.857c0-1.25 1.036-2.286 2.286-2.286h9.143c1.25 0 2.286 1.036 2.286 2.286zM29.714 18.286v6.857c0 1.25-1.036 2.286-2.286 2.286h-9.143c-1.25 0-2.286-1.036-2.286-2.286v-6.857c0-1.25 1.036-2.286 2.286-2.286h9.143c1.25 0 2.286 1.036 2.286 2.286zM29.714 4.571v6.857c0 1.25-1.036 2.286-2.286 2.286h-9.143c-1.25 0-2.286-1.036-2.286-2.286v-6.857c0-1.25 1.036-2.286 2.286-2.286h9.143c1.25 0 2.286 1.036 2.286 2.286z"></path>');
+document.writeln('</symbol>');
+
+document.writeln('</svg>');
